327f129ef991c01ad79492e015897969d9dcb9a7
mst <mst@touk.pl>
2015-07-02 11:52:22 +0200

    Added base class for API tests

---
+++ api/src/test/java/ApiTestBase.java
@@ -0,0 +1,20 @@
+import org.broadleafcommerce.core.catalog.service.CatalogService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+
+public abstract class ApiTestBase {
+
+    public static final String CATEGORIES_URL = "http://localhost:{port}/catalog/categories";
+
+
+    @Resource(name="blCatalogService")
+    protected CatalogService catalogService;
+
+    protected RestTemplate restTemplate = new RestTemplate();
+
+    protected final int serverPort = 8080;
+
+}
