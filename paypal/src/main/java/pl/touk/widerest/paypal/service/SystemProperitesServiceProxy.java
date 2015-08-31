323ab5b637065806af45a52d237f5173a95fd584
Przemyslaw Kopanski <pkp@touk.pl>
2015-08-31 14:37:10 +0200

    Minor refactor of SystemPropertiesServiceProxy

--- paypal/src/main/java/pl/touk/widerest/paypal/service/SystemProperitesServiceProxy.java
+++ paypal/src/main/java/pl/touk/widerest/paypal/service/SystemProperitesServiceProxy.java
@@ -1,12 +1,11 @@
 package pl.touk.widerest.paypal.service;
 
-
-import org.apache.velocity.exception.ResourceNotFoundException;
 import org.broadleafcommerce.common.config.dao.SystemPropertiesDao;
 import org.broadleafcommerce.common.config.domain.SystemProperty;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Optional;
 
 @Service("wdSystemProperties")
 public class SystemProperitesServiceProxy {
@@ -18,6 +17,7 @@ public class SystemProperitesServiceProxy {
     public final static String SECRET = "paypal_secret";
 
     public SystemProperty getSystemPropertyByName(String name) {
+        // systemProperitesDao.readSystemPropertyByName behaves in unexpected way
         return systemPropertiesDao.readAllSystemProperties().stream()
                 .filter(e -> e.getName().equals(name))
                 .findAny()
@@ -26,11 +26,9 @@ public class SystemProperitesServiceProxy {
 
     public SystemProperty setOrUpdatePropertyByName(String name, String value) {
 
-        if(getSystemPropertyByName(name) != null) {
-            systemPropertiesDao.deleteSystemProperty(
-                    systemPropertiesDao.readSystemPropertyByName(name)
-            );
-        }
+        Optional.ofNullable(getSystemPropertyByName(name))
+                .ifPresent(systemPropertiesDao::deleteSystemProperty);
+
         SystemProperty prop = systemPropertiesDao.createNewSystemProperty();
         prop.setName(name);
         prop.setValue(value);
