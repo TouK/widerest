dce441b7260232503cb4c72a006d3313d38a1b99
Marcin Stelmaszczyk <mst@touk.pl>
2015-10-15 09:28:56 +0200

    Two endpoints for changing admins' passwords.

--- api/src/main/java/pl/touk/widerest/swagger/SwaggerConfig.java
+++ api/src/main/java/pl/touk/widerest/swagger/SwaggerConfig.java
@@ -63,7 +63,9 @@ public class SwaggerConfig {
     }
 
     private Predicate<String> paths() {
-        return or(regex("/catalog/.*"), regex("/orders.*"), regex("/customers.*"), regex("/paypal.*"), regex("/tenant.*"), regex("/settings.*"));
+        return or(regex("/catalog/.*"), regex("/orders.*"), regex("/customers.*"),
+                regex("/paypal.*"), regex("/tenant.*"), regex("/settings.*"),
+                regex("/manager.*"));
     }
 
     @Bean
