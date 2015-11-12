02e4cbcd4b0737b820b2aed38a16335efd7c1161
Tomasz Wielga <tw@touk.pl>
2015-11-12 20:53:58 +0100

    swagger https fix

--- api/src/main/java/pl/touk/widerest/swagger/SwaggerConfig.java
+++ api/src/main/java/pl/touk/widerest/swagger/SwaggerConfig.java
@@ -18,6 +18,7 @@ import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.UiConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
@@ -65,6 +66,11 @@ public class SwaggerConfig {
                 )
                 .forPaths(PathSelectors.regex("/.*"))
                 .build();
+    }
+
+    @Bean
+    public UiConfiguration uiConfiguration() {
+        return new UiConfiguration("https://online.swagger.io/validator/");
     }
 
     @Bean
