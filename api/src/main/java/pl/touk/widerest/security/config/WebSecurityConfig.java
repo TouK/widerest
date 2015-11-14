ae2126331ed3cfc87c73f32c80808de6947c88dc
Tomasz Wielga <tw@touk.pl>
2015-11-14 01:32:21 +0100

    spring boot update, x-frame-options disabled

--- api/src/main/java/pl/touk/widerest/security/config/WebSecurityConfig.java
+++ api/src/main/java/pl/touk/widerest/security/config/WebSecurityConfig.java
@@ -80,6 +80,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .headers().frameOptions().disable()
                 .authorizeRequests()
                     .anyRequest().permitAll()
                     .and()
