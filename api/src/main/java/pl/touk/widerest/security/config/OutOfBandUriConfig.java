ba9f1b10d8a99dd7c4138344bbd94e6f725ed88c
Tomasz Wielga <tw@touk.pl>
2015-11-18 13:10:01 +0100

    oauth "out of band" uri handling

---
+++ api/src/main/java/pl/touk/widerest/security/config/OutOfBandUriConfig.java
@@ -0,0 +1,63 @@
+package pl.touk.widerest.security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.mvc.annotation.ResponseStatusExceptionResolver;
+import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
+import org.springframework.web.servlet.mvc.method.annotation.JsonViewResponseBodyAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+import org.springframework.web.servlet.mvc.support.DefaultHandlerExceptionResolver;
+import pl.touk.widerest.security.oauth2.OutOfBandUriHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class OutOfBandUriConfig extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    HttpMessageConverters messageConverters;
+
+    @Autowired
+    WebMvcConfigurationSupport webMvcConfigurationSupport;
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Bean
+    HandlerMethodReturnValueHandler oAuth2OutOfBandUriHandler(RequestMappingHandlerAdapter adapter) {
+        OutOfBandUriHandler handler = new OutOfBandUriHandler(adapter);
+        return handler;
+    };
+
+    @Override
+    public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
+        ExceptionHandlerExceptionResolver exceptionHandlerExceptionResolver = new ExceptionHandlerExceptionResolver();
+        exceptionHandlerExceptionResolver.setContentNegotiationManager(webMvcConfigurationSupport.mvcContentNegotiationManager());
+        exceptionHandlerExceptionResolver.setMessageConverters(this.messageConverters.getConverters());
+            List<ResponseBodyAdvice<?>> interceptors = new ArrayList<ResponseBodyAdvice<?>>();
+            interceptors.add(new JsonViewResponseBodyAdvice());
+            exceptionHandlerExceptionResolver.setResponseBodyAdvice(interceptors);
+        exceptionHandlerExceptionResolver.setApplicationContext(this.applicationContext);
+        exceptionHandlerExceptionResolver.afterPropertiesSet();
+        new OutOfBandUriHandler(exceptionHandlerExceptionResolver);
+        exceptionResolvers.add(exceptionHandlerExceptionResolver);
+
+        ResponseStatusExceptionResolver responseStatusExceptionResolver = new ResponseStatusExceptionResolver();
+        responseStatusExceptionResolver.setMessageSource(this.applicationContext);
+        exceptionResolvers.add(responseStatusExceptionResolver);
+
+        exceptionResolvers.add(new DefaultHandlerExceptionResolver());
+
+    }
+
+
+}
--- app/src/test/java/pl/touk/widerest/AuthorizationTest.java
+++ app/src/test/java/pl/touk/widerest/AuthorizationTest.java
@@ -2,11 +2,9 @@ package pl.touk.widerest;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.methods.RequestBuilder;
-import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -17,17 +15,26 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.client.ClientHttpRequest;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.resource.BaseOAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpMessageConverterExtractor;
 import pl.touk.widerest.base.ApiTestBase;
+import pl.touk.widerest.security.oauth2.OutOfBandUriHandler;
 import pl.touk.widerest.security.oauth2.Scope;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.Arrays;
+import java.util.Map;
 import java.util.Optional;
 
 @SpringApplicationConfiguration(classes = Application.class)
@@ -110,15 +117,16 @@ public class AuthorizationTest extends ApiTestBase {
     }
 
     protected void whenAuthorizationRequestedFor(Scope scope) throws IOException {
-        HttpUriRequest request = RequestBuilder.get()
-                .setUri("http://localhost:" + serverPort + "/oauth/authorize?client_id=default&response_type=token&redirect_uri=%2F" + (scope != null ? "&scope=" + scope : ""))
-                .build();
+        HttpComponentsClientHttpRequestFactory httpRequestFactory = new HttpComponentsClientHttpRequestFactory(authorizationServerClient);
+        ClientHttpRequest request = httpRequestFactory.createRequest(
+                URI.create("http://localhost:" + serverPort + "/oauth/authorize?client_id=default&response_type=token&redirect_uri=" + OutOfBandUriHandler.OOB_URI + (scope != null ? "&scope=" + scope : "")),
+                HttpMethod.GET
+        );
 
-        try (CloseableHttpResponse response = authorizationServerClient.execute(request)) {
-            org.apache.http.HttpEntity entity = response.getEntity();
-            Optional<String> accessToken = Optional.ofNullable(response.getFirstHeader("Location"))
-                    .flatMap(header -> URLEncodedUtils.parse(URI.create(header.getValue().replace('#', '?')), "UTF-8").stream().filter(param -> param.getName().equals("access_token")).findAny())
-                    .map(NameValuePair::getValue);
+        try (ClientHttpResponse response = request.execute()) {
+            HttpMessageConverterExtractor<Map> e = new HttpMessageConverterExtractor(Map.class, Arrays.asList(new MappingJackson2HttpMessageConverter()));
+            Map<String, String> map = e.extractData(response);
+            Optional<String> accessToken = Optional.ofNullable(map.get("access_token"));
             restTemplate.getOAuth2ClientContext().setAccessToken(accessToken.map(DefaultOAuth2AccessToken::new).orElse(null));
         }
     }
