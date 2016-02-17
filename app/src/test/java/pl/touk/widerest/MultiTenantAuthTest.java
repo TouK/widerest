2da3bb5e65c62656b6e929308a90cdc324e09ab2
Tomasz Wielga <tw@touk.pl>
2016-02-17 11:23:59 +0100

    after merge fixes

--- app/src/test/java/pl/touk/widerest/MultiTenantAuthTest.java
+++ app/src/test/java/pl/touk/widerest/MultiTenantAuthTest.java
@@ -39,7 +39,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@SpringApplicationConfiguration(classes = Application.class, initializers = Application.ContextInitializer.class)
+@SpringApplicationConfiguration(classes = Application.class, initializers = Application.ContextInitializer.class, inheritInitializers = false)
 @RunWith(SpringJUnit4ClassRunner.class)
 @Slf4j
 public class MultiTenantAuthTest extends ApiTestBase {
