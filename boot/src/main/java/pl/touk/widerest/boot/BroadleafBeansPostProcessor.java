aec9785440fb461218c563dedb711fc3c3b6ae2b
Tomasz Wielga <tw@touk.pl>
2016-03-21 18:26:17 +0100

    braodleaf vs spring boot filters fix

--- boot/src/main/java/pl/touk/widerest/boot/BroadleafBeansPostProcessor.java
+++ boot/src/main/java/pl/touk/widerest/boot/BroadleafBeansPostProcessor.java
@@ -1,39 +1,57 @@
 package pl.touk.widerest.boot;
 
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
 
-import javax.servlet.Filter;
 import java.util.Arrays;
 
 public class BroadleafBeansPostProcessor implements BeanFactoryPostProcessor {
 
     @Override
-    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-        registerAliases(beanFactory);
-        removeWebFilters((DefaultListableBeanFactory) beanFactory);
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory bf) throws BeansException {
 
-        if (beanFactory.containsBeanDefinition("jpaVendorAdapter")) {
-            ((DefaultListableBeanFactory) beanFactory).removeBeanDefinition("blJpaVendorAdapter");
-            beanFactory.registerAlias("jpaVendorAdapter", "blJpaVendorAdapter");
-        }
+        DefaultListableBeanFactory beanFactory = (DefaultListableBeanFactory) bf;
+
+        disableAutomaticBroadleafFiltersRegistration(beanFactory);
+        registerDatasourceAliases(beanFactory);
+        removeDuplicatedJpaAdapter(beanFactory);
     }
 
-    private static void registerAliases(ConfigurableListableBeanFactory beanFactory) {
+    protected void registerDatasourceAliases(DefaultListableBeanFactory beanFactory) {
         beanFactory.registerAlias("dataSource", "webDS");
         beanFactory.registerAlias("dataSource", "webStorageDS");
         beanFactory.registerAlias("dataSource", "webSecureDS");
     }
 
-    private static void removeWebFilters(DefaultListableBeanFactory beanFactory) {
-        Arrays.stream(beanFactory.getBeanNamesForType(Filter.class))
-                .filter(beanName -> {
-                    final String beanClassName = beanFactory.getBeanDefinition(beanName).getBeanClassName();
-                    return beanClassName != null && beanClassName.startsWith("org.broadleafcommerce");
-                })
-                .forEach(beanFactory::removeBeanDefinition);
+    protected void disableAutomaticBroadleafFiltersRegistration(DefaultListableBeanFactory beanFactory) {
+        Arrays.stream(beanFactory.getBeanNamesForType(javax.servlet.Filter.class))
+                .filter(name -> name.startsWith("bl"))
+                .forEach(name -> {
+                    BeanDefinition definition = BeanDefinitionBuilder
+                            .genericBeanDefinition(FilterRegistrationBean.class)
+                            .setScope(BeanDefinition.SCOPE_SINGLETON)
+                            .addConstructorArgReference(name)
+                            .addConstructorArgValue(new ServletRegistrationBean[]{})
+                            .addPropertyValue("enabled", false)
+                            .getBeanDefinition();
+
+                    beanFactory.registerBeanDefinition(name + "FilterRegistrationBean",
+                            definition);
+                });
 
     }
+
+    protected void removeDuplicatedJpaAdapter(DefaultListableBeanFactory beanFactory) {
+        if(beanFactory.containsBeanDefinition("jpaVendorAdapter")) {
+            ((DefaultListableBeanFactory)beanFactory).removeBeanDefinition("blJpaVendorAdapter");
+            beanFactory.registerAlias("jpaVendorAdapter", "blJpaVendorAdapter");
+        }
+    }
+
 }
