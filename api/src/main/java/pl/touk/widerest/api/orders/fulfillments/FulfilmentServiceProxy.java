6b6396af2d3630818857dc92d374d8f767f9ed1d
Tomasz Wielga <tw@touk.pl>
2016-10-07 11:20:18 +0200

    an item added to the temporary fulfilment group when estimating the cost of fulfillment

--- api/src/main/java/pl/touk/widerest/api/orders/fulfillments/FulfilmentServiceProxy.java
+++ api/src/main/java/pl/touk/widerest/api/orders/fulfillments/FulfilmentServiceProxy.java
@@ -2,33 +2,32 @@ package pl.touk.widerest.api.orders.fulfillments;
 
 import org.broadleafcommerce.common.money.Money;
 import org.broadleafcommerce.common.vendor.service.exception.FulfillmentPriceException;
+import org.broadleafcommerce.common.web.BroadleafRequestContext;
 import org.broadleafcommerce.core.catalog.domain.Category;
 import org.broadleafcommerce.core.catalog.domain.Product;
 import org.broadleafcommerce.core.catalog.domain.Sku;
+import org.broadleafcommerce.core.order.dao.FulfillmentGroupDao;
+import org.broadleafcommerce.core.order.dao.FulfillmentGroupItemDao;
+import org.broadleafcommerce.core.order.dao.OrderDao;
+import org.broadleafcommerce.core.order.dao.OrderItemDao;
 import org.broadleafcommerce.core.order.domain.DiscreteOrderItem;
 import org.broadleafcommerce.core.order.domain.FulfillmentGroup;
+import org.broadleafcommerce.core.order.domain.FulfillmentGroupItem;
 import org.broadleafcommerce.core.order.domain.FulfillmentOption;
 import org.broadleafcommerce.core.order.domain.Order;
 import org.broadleafcommerce.core.order.service.FulfillmentGroupService;
 import org.broadleafcommerce.core.order.service.FulfillmentOptionService;
-import org.broadleafcommerce.core.order.service.OrderService;
 import org.broadleafcommerce.core.order.service.type.FulfillmentType;
+import org.broadleafcommerce.core.order.service.type.OrderItemType;
 import org.broadleafcommerce.core.pricing.service.FulfillmentPricingService;
-import org.broadleafcommerce.core.pricing.service.exception.PricingException;
-import org.broadleafcommerce.profile.core.domain.Address;
-import org.broadleafcommerce.profile.core.domain.Customer;
-import org.broadleafcommerce.profile.core.service.CustomerService;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import pl.touk.widerest.api.common.AddressConverter;
 
 import javax.annotation.Resource;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 
@@ -37,25 +36,25 @@ import java.util.Set;
 public class FulfilmentServiceProxy {
 
     @Resource(name = "blFulfillmentOptionService")
-    private FulfillmentOptionService fulfillmentOptionService;
+    protected FulfillmentOptionService fulfillmentOptionService;
 
     @Resource(name = "blFulfillmentGroupService")
-    private FulfillmentGroupService fulfillmentGroupService;
+    protected FulfillmentGroupService fulfillmentGroupService;
 
     @Resource(name = "blFulfillmentPricingService")
     private FulfillmentPricingService fulfillmentPricingService;
 
-    @Resource(name = "blOrderService")
-    private OrderService orderService;
+    @Resource(name = "blOrderDao")
+    private OrderDao orderDao;
 
-    @Resource(name = "blCustomerService")
-    private CustomerService customerService;
+    @Resource(name = "blOrderItemDao")
+    private OrderItemDao orderItemDao;
 
-    @Resource
-    private AddressConverter addressConverter;
+    @Resource(name = "blFulfillmentGroupDao")
+    private FulfillmentGroupDao fulfillmentGroupDao;
 
-    public void init() {
-    }
+    @Resource(name = "blFulfillmentGroupItemDao")
+    private FulfillmentGroupItemDao fulfillmentGroupItemDao;
 
     @Cacheable(key="#type?.type ?: '_null_'")
     public Map<? extends FulfillmentOption, Money> readFulfillmentOptionsWithPricesAvailableByFulfillmentType(FulfillmentType type) throws FulfillmentPriceException {
@@ -63,13 +62,9 @@ public class FulfilmentServiceProxy {
                 ? fulfillmentOptionService.readAllFulfillmentOptionsByFulfillmentType(type)
                 : fulfillmentOptionService.readAllFulfillmentOptions();
 
-        FulfillmentGroup emptyFulfillmentGroup = fulfillmentGroupService.createEmptyFulfillmentGroup();
-        Customer customer = customerService.createCustomer();
-        Order newCartForCustomer = orderService.createNewCartForCustomer(customer);
-
 
         return fulfillmentPricingService.estimateCostForFulfillmentGroup(
-                fulfillmentGroupService.createEmptyFulfillmentGroup(),
+                createTemporaryFulfillmentGroup(),
                 new HashSet(fulfillmentOptions)
         ).getFulfillmentOptionPrices();
     }
@@ -91,9 +86,43 @@ public class FulfilmentServiceProxy {
         fulfillmentOptions.removeAll(product.getDefaultSku().getExcludedFulfillmentOptions());
 
         return fulfillmentPricingService.estimateCostForFulfillmentGroup(
-                fulfillmentGroupService.createEmptyFulfillmentGroup(),
+                createTemporaryFulfillmentGroupWithSingleItem(product),
                 new HashSet(fulfillmentOptions)
         ).getFulfillmentOptionPrices();
+    }
+
+    private FulfillmentGroup createTemporaryFulfillmentGroup() {
+
+        FulfillmentGroup fulfillmentGroup = fulfillmentGroupDao.create();
+        Order order = orderDao.create();
+        if (BroadleafRequestContext.getBroadleafRequestContext() != null) {
+            order.setCurrency(BroadleafRequestContext.getBroadleafRequestContext().getBroadleafCurrency());
+            order.setLocale(BroadleafRequestContext.getBroadleafRequestContext().getLocale());
+        }
+        fulfillmentGroup.setOrder(order);
+        order.getFulfillmentGroups().add(fulfillmentGroup);
+
+        return fulfillmentGroup;
+    }
+
+    private FulfillmentGroup createTemporaryFulfillmentGroupWithSingleItem(Product product) {
+
+        FulfillmentGroup fulfillmentGroup = createTemporaryFulfillmentGroup();
+
+        DiscreteOrderItem orderItem = (DiscreteOrderItem) orderItemDao.create(OrderItemType.DISCRETE);
+        orderItem.setSku(product.getDefaultSku());
+        orderItem.setOrder(fulfillmentGroup.getOrder());
+        orderItem.setPrice(product.getDefaultSku().getPrice());
+
+        FulfillmentGroupItem fgi = fulfillmentGroupItemDao.create();
+        fgi.setFulfillmentGroup(fulfillmentGroup);
+        fgi.setOrderItem(orderItem);
+        fgi.setQuantity(1);
+
+        fulfillmentGroup.addFulfillmentGroupItem(fgi);
+
+        return fulfillmentGroup;
+
     }
 
     public Set<FulfillmentOption> findFulfillmentOptionsForProductsInFulfillmentGroup(FulfillmentGroup fulfillmentGroup) {
@@ -118,40 +147,6 @@ public class FulfilmentServiceProxy {
         }
 
         return fulfillmentOptions;
-    }
-
-    public FulfillmentGroup updateFulfillmentOption(FulfillmentGroup fulfillmentGroup, long fulfillmentOptionId) throws PricingException {
-        Set<FulfillmentOption> allowedFulfillmentOptions = findFulfillmentOptionsForProductsInFulfillmentGroup(fulfillmentGroup);
-        FulfillmentOption fulfillmentOption = fulfillmentOptionService.readFulfillmentOptionById(fulfillmentOptionId);
-
-        if (fulfillmentOption == null) {
-            throw new UnknownFulfillmentOptionException();
-        }
-        if (!allowedFulfillmentOptions.contains(fulfillmentOption)) {
-            throw new FulfillmentOptionNotAllowedException();
-        }
-
-        return fulfillmentGroup;
-    }
-
-    @Transactional
-    public Optional<Address> getFulfillmentAddress(Order order) {
-        return Optional.ofNullable(fulfillmentGroupService.getFirstShippableFulfillmentGroup(order))
-                .filter(Objects::nonNull)
-                .map(FulfillmentGroup::getAddress);
-    }
-
-    public Order updateFulfillmentAddress(Order order, Address address) throws PricingException {
-        FulfillmentGroup shippableFulfillmentGroup = fulfillmentGroupService.getFirstShippableFulfillmentGroup(order);
-
-        if(shippableFulfillmentGroup != null) {
-            shippableFulfillmentGroup.setAddress(address);
-            order = orderService.save(order, true);
-    		
-        } else {
-            throw new NotShippableException();
-        }
-        return order;
     }
 
 }
